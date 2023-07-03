2aaf2c78f2530399fe93260edc0384f829cecaca
Sumanone8 <sumanjhabhl@gmail.com>
2023-07-03 20:11:34 +0530

    Suman : Welcome Massage to EWP101

---
+++ PracticeClass.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PracticeSheet
+{
+    class PracticeClass
+    {
+        public void DoPractice()
+        {
+            Console.WriteLine(" Welcome to Employment Wage Problem 101");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace PracticeSheet
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello Suman! Welcome.");
+            Console.WriteLine("Please enter the purpose of your practice:");
+            string purpose = Console.ReadLine();
+            Console.WriteLine("Today we are going to practice " + purpose);
+            Console.WriteLine("Let's begin!");
+
+            PracticeClass practice = new PracticeClass();
+            practice.DoPractice();
+
+            Console.WriteLine("Practice session complete. Goodbye!");
+            Console.ReadLine();
+        }
+    }
+}
