0411980d8ad2ba4c1e884ae127bd440a747e72c7
Sumanone8 <sumanjhabhl@gmail.com>
2023-07-04 10:16:18 +0530

    Suman : Modified UC2 solution

--- PracticeClass.cs
+++ PracticeClass.cs
@@ -6,15 +6,25 @@ namespace PracticeSheet
     {
         public void DoPractice()
         {
-            Console.WriteLine("Welcome to Employment Wage Problem 101");
+            Console.WriteLine("Welcome to the Employee Wage Calculator!");
 
-            //UC2 - Daily Wage of a Employee
-            int hourlyRate = 20;
-            int hoursWorked = 8;
+            // Generate a random number to determine whether the employee is present or absent
+            Random random = new Random();
+            bool isPresent = random.Next(0, 2) == 0;
 
-            int dailyWage = hourlyRate * hoursWorked;
-
-            Console.WriteLine("Daily Employee Wage: $" + dailyWage);
+            if (isPresent)
+            {
+                int fullTimeWorkHours = 8;
+                int hourlyPayRate = 20;
+                int dailyWage = fullTimeWorkHours * hourlyPayRate;
+                Console.WriteLine("Employee is present.");
+                Console.WriteLine("Daily wage: $" + dailyWage);
+            }
+            else
+            {
+                Console.WriteLine("Employee is absent.");
+                Console.WriteLine("No daily wage earned.");
+            }
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -7,9 +7,6 @@ namespace PracticeSheet
         static void Main()
         {
             Console.WriteLine("Hello Suman! Welcome.");
-            Console.WriteLine("Please enter the purpose of your practice:");
-            string purpose = Console.ReadLine();
-            Console.WriteLine("Today we are going to practice " + purpose);
             Console.WriteLine("Let's begin!");
 
             PracticeClass practice = new PracticeClass();
