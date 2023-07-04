22194937f357b73177e94311411f90cd0f75a262
Sumanone8 <sumanjhabhl@gmail.com>
2023-07-04 10:34:02 +0530

    Suman : UC4 solved

--- PracticeClass.cs
+++ PracticeClass.cs
@@ -8,30 +8,40 @@ namespace PracticeSheet
         {
             Console.WriteLine("Welcome to the Employee Wage Calculator!");
 
-            // Generate a random number to determine whether the employee is present or absent
+            // Generate a random number to determine the work type: 0 for full-time, 1 for part-time, 2 for absent
             Random random = new Random();
-            int attendance = random.Next(0, 3);
+            int workType = random.Next(0, 3);
 
-            if (attendance == 1)
+            int workHours;
+            int hourlyPayRate;
+            int dailyWage;
+
+            switch (workType)
             {
-                int fullTimeWorkHours = 8;
-                int hourlyPayRate = 20;
-                int dailyWage = fullTimeWorkHours * hourlyPayRate;
-                Console.WriteLine("Employee is present.");
-                Console.WriteLine("Daily wage: $" + dailyWage);
-            }
-            else if (attendance == 2)
-            {
-                int PartTimeWorkHours = 4;
-                int hourlyPayRate = 20;
-                int dailyWage = PartTimeWorkHours * hourlyPayRate;
-                Console.WriteLine("Employee is present part-time.");
-                Console.WriteLine("Daily wage: $" + dailyWage);
-            }
-            else
-            {
-                Console.WriteLine("Employee is absent.");
-                Console.WriteLine("No daily wage earned.");
+                case 0: // Full-time
+                    workHours = 8;
+                    hourlyPayRate = 20;
+                    dailyWage = workHours * hourlyPayRate;
+                    Console.WriteLine("Employee is present and working full-time.");
+                    Console.WriteLine("Daily wage: $" + dailyWage);
+                    break;
+                case 1: // Part-time
+                    workHours = 4;
+                    hourlyPayRate = 20;
+                    dailyWage = workHours * hourlyPayRate;
+                    Console.WriteLine("Employee is present and working part-time.");
+                    Console.WriteLine("Daily wage: $" + dailyWage);
+                    break;
+
+                case 2: // Absent
+                    Console.WriteLine("Employee is absent.");
+                    Console.WriteLine("No daily wage earned.");
+                    break;
+
+                default:
+                    Console.WriteLine("Invalid work type.");
+                    break;
+
             }
         }
     }
